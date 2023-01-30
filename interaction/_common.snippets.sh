2c9056092c63a4bf45756862e1d375a8216e14cd
Aurélien Passion <aurelien.passion@gmail.com>
2023-01-30 14:20:07 +0100

    adjust gas

---
+++ interaction/_common.snippets.sh
@@ -0,0 +1,114 @@
+##
+# Info
+##
+
+echo "Proxy: ${PROXY}"
+echo "SC address: ${SC_ADDRESS:-Not deployed}"
+
+##
+# Owner endpoints
+##
+
+fund() {
+    read -p 'Rewards token ID: ' REWARDS_TOKEN_ID
+    read -p "Amount (in weis - no float): " AMOUNT
+
+    REWARDS_TOKEN_ID="0x$(echo -n "${REWARDS_TOKEN_ID}" | xxd -ps)"
+    METHOD="0x$(echo -n "fund" | xxd -ps)"
+
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce --keyfile=${KEYFILE} --gas-limit=5000000 \
+        --function="ESDTTransfer" --arguments ${REWARDS_TOKEN_ID} ${AMOUNT} ${METHOD} \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
+
+setRewardsDuration() {
+    read -p "Duration (sec): " DURATION
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce --keyfile=${KEYFILE} --gas-limit=4000000 \
+        --function="setRewardsDuration" \
+        --arguments ${DURATION} \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
+
+##
+# Views
+##
+
+getBalanceOf() {
+    read -p "Address: " ADDRESS
+    ADDRESS="0x$(erdpy wallet bech32 --decode ${ADDRESS})"
+    
+    erdpy --verbose contract query ${SC_ADDRESS} \
+        --function "getBalanceOf" --arguments "${ADDRESS}" \
+        --proxy=${PROXY}
+}
+
+getFinishAt() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getFinishAt" --proxy=${PROXY}
+}
+
+getPendingRewards() {
+    read -p "Address: " ADDRESS
+    ADDRESS="0x$(erdpy wallet bech32 --decode ${ADDRESS})"
+    
+    erdpy --verbose contract query ${SC_ADDRESS} \
+        --function "getPendingRewards" --arguments "${ADDRESS}" \
+        --proxy=${PROXY}
+}
+
+getRewardsDuration() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getRewardsDuration" --proxy=${PROXY}
+}
+
+getRewardsToken() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getRewardsToken" --proxy=${PROXY}
+}
+
+getRewardPerSecond() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getRewardPerSecond" --proxy=${PROXY}
+}
+
+getStakingToken() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getStakingToken" --proxy=${PROXY}
+}
+
+getTotalStaked() {
+    erdpy --verbose contract query ${SC_ADDRESS} --function "getTotalStaked" --proxy=${PROXY}
+}
+
+##
+# Public endpoints
+##
+
+claim() {
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce \
+        --pem=$1 --gas-limit=6000000 \
+        --function="claim" \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
+
+exit() {
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce \
+        --pem=$1 --gas-limit=8000000 \
+        --function="exit" \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
+
+stake() {
+    read -p "Token identifier: " TOKEN_IDENTIFIER
+    read -p "Amount (in weis - no float): " AMOUNT
+
+    TOKEN_IDENTIFIER="0x$(echo -n "${TOKEN_IDENTIFIER}" | xxd -ps)"
+    METHOD="0x$(echo -n "stake" | xxd -ps)"
+
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce --pem=$1 --gas-limit=6000000 \
+        --function="ESDTTransfer" --arguments ${TOKEN_IDENTIFIER} ${AMOUNT} ${METHOD} \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
+
+withdraw() {
+    read -p "Amount (in weis - no float): " AMOUNT
+
+    erdpy --verbose contract call ${SC_ADDRESS} --recall-nonce --pem=$1 --gas-limit=6000000 \
+        --function="withdraw" --arguments ${AMOUNT} \
+        --proxy=${PROXY} --chain=${CHAIN} --send || return
+}
---
+++ interaction/devnet.snippets.sh
@@ -0,0 +1,55 @@
+PROJECT=..
+KEYFILE="../../wallets/deployer.json"
+PROXY=https://devnet-gateway.elrond.com
+SC_ADDRESS=$(erdpy data load --key=address-devnet)
+CHAIN=D
+SCRIPT_DIR=$(dirname $0)
+
+source "${SCRIPT_DIR}/_common.snippets.sh"
+
+deploy() {
+    echo 'You are about to deploy SC on devnet (Ctrl-C to abort)'
+    read answer
+
+    read -p 'Stake token ID: ' STAKE_TOKEN_ID
+    read -p 'Rewards token ID: ' REWARDS_TOKEN_ID
+
+    STAKE_TOKEN_ID="0x$(echo -n "${STAKE_TOKEN_ID}" | xxd -ps)"
+    REWARDS_TOKEN_ID="0x$(echo -n "${REWARDS_TOKEN_ID}" | xxd -ps)"
+
+    erdpy --verbose contract deploy --project=${PROJECT} --metadata-payable \
+        --keyfile=${KEYFILE} --gas-limit=50000000 --outfile="deploy-devnet.interaction.json" \
+        --arguments "${STAKE_TOKEN_ID}" "${REWARDS_TOKEN_ID}" \
+        --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
+
+    SC_ADDRESS=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['contractAddress']")
+
+    erdpy data store --key=address-devnet --value=${SC_ADDRESS}
+
+    echo ""
+    echo "Smart contract address: ${SC_ADDRESS}"
+}
+
+upgrade() {
+    echo 'You are about to upgrade current SC on devnet (Ctrl-C to abort)'
+    read answer
+
+    read -p 'Stake token ID: ' STAKE_TOKEN_ID
+    read -p 'Rewards token ID: ' REWARDS_TOKEN_ID
+
+    STAKE_TOKEN_ID="0x$(echo -n "${STAKE_TOKEN_ID}" | xxd -ps)"
+    REWARDS_TOKEN_ID="0x$(echo -n "${REWARDS_TOKEN_ID}" | xxd -ps)"
+
+    erdpy --verbose contract upgrade --project=${PROJECT} --metadata-payable \
+        --keyfile=${KEYFILE} --gas-limit=50000000 --outfile="deploy-devnet.interaction.json" \
+        --arguments "${STAKE_TOKEN_ID}" "${REWARDS_TOKEN_ID}" \
+        --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send ${SC_ADDRESS} || return
+
+    echo ""
+    echo "Smart contract upgraded: ${SC_ADDRESS}"
+}
+
+CMD=$1
+shift
+
+$CMD $*

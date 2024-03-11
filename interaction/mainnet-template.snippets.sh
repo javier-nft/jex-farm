5bafca71ec6e280cfde7d4eb82f19749985a7a6e
Aurélien Passion <aurelien.passion@gmail.com>
2024-03-11 11:31:37 +0100

    add deployment script for mainnet

---
+++ interaction/mainnet-template.snippets.sh
@@ -0,0 +1,43 @@
+BYTECODE=../output-docker/jex-sc-farm/jex-sc-farm.wasm
+PROXY=https://gateway.multiversx.com
+SC_ADDRESS=$(mxpy data load --key=address-mainnet-template)
+CHAIN=1
+SCRIPT_DIR=$(dirname $0)
+
+source "${SCRIPT_DIR}/_common.snippets.sh"
+
+# Reproducible build using:
+# mxpy contract reproducible-build --docker-image="multiversx/sdk-rust-contract-builder:v6.1.1"
+deploy() {
+    echo 'You are about to deploy SC on mainnet (Ctrl-C to abort)'
+    read answer
+
+    mxpy contract deploy --bytecode=${BYTECODE} \
+        --keyfile=${1} --gas-limit=50000000 --outfile="deploy-mainnet.interaction.json" \
+        --arguments "0x" "0x" \
+        --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
+
+    SC_ADDRESS=$(cat deploy-mainnet.interaction.json | jq -r .contractAddress)
+
+    mxpy data store --key=address-mainnet-template --value=${SC_ADDRESS}
+
+    echo ""
+    echo "Smart contract address: ${SC_ADDRESS}"
+}
+
+upgrade() {
+    echo 'You are about to upgrade current SC on mainnet (Ctrl-C to abort)'
+    read answer
+
+    mxpy contract upgrade --bytecode=${BYTECODE} \
+        --keyfile=${1} --gas-limit=50000000 --outfile="deploy-mainnet.interaction.json" \
+        --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send ${SC_ADDRESS} || return
+
+    echo ""
+    echo "Smart contract upgraded: ${SC_ADDRESS}"
+}
+
+CMD=$1
+shift
+
+$CMD $*
